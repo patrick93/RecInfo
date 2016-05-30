ee1a9f6b2e076f0266232eb9ad1510e53734647e
Patrick Gazaneo <pck1993@gmail.com>
2016-05-30 01:20:05 -0300

    script para pegar os resultados retornados pelo google

---
+++ google_api.py
@@ -0,0 +1,16 @@
+import requests
+
+API_KEY = "AIzaSyAhrht1pkJNuAw3sQuUFh34v-Yoaa9adhI"
+CX = "010532648492414585372:wgsvyzd_u2e"
+
+def get_links(query):
+	links = []
+	url = "https://www.googleapis.com/customsearch/v1"
+	params = {'key': API_KEY, 'cx': CX, 'q': query}
+	response = requests.get(url, params=params)
+	content = response.json()
+	for item in content['items']:
+		links.append(item['link'])
+	return links
+
+print(get_links("lectures"))
